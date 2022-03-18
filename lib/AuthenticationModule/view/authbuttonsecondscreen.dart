117a10306b1190d9bef2e8cefef44ea693bc5b10
Imtiaz1245 <imtiaz12685@gmail.com>
2022-03-18 12:54:01 +0500

    message

--- lib/AuthenticationModule/view/authbuttonsecondscreen.dart
+++ lib/AuthenticationModule/view/authbuttonsecondscreen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 class AuthButtonScreen2 extends StatelessWidget {
-
- final  childe;
-  const AuthButtonScreen2({Key? key, required this.childe, }) : super(key: key);
+  final onpress;
+  final  childe;
+  const AuthButtonScreen2({Key? key, required this.childe, this.onpress, }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(onPressed: () {  },
+    return MaterialButton(onPressed: onpress,
       child: childe,);
   }
 }
